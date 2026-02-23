b051ef271a68ac9e3c062d9dc26731af5bc22b6b
Kiisi <kiisifelix06@gmail.com>
2026-02-23 02:24:28 +0100

    feat: vercel json

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), react(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    react(),
+    tsconfigPaths(),
+  ],
 });
